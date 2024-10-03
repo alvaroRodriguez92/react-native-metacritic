0c4b6f708ea0f06209fc7b30c21b8cbdf6a599f0
alvaroRodriguez92 <alvaros.rodriguez92@gmail.com>
2024-10-03 19:45:47 +0200

    first commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  Image,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TouchableHighlight,
+  ScrollView,
+  SafeAreaView
+} from "react-native";
+import { getLatestGames } from "./lib/metacritic";
+import {useState, useEffect} from "react";
+import  Constants from "expo-constants";
 
 export default function App() {
+  const [games, setGames] = useState<any[]>([]);
+
+  useEffect(() => {
+    getLatestGames().then((games) => setGames(games));
+  }, []);
+ 
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <ScrollView>
+      {games.map((game) => (
+        <View key={game.slug} style={styles.card}>
+          <Image source={{uri: game.image}}
+          style={styles.image}/>
+          <Text style={styles.title}>{game.title}</Text>
+          <Text style={styles.description}>{game.description}</Text>
+          <Text style={styles.score}>{game.score}</Text>
+        </View>
+      ))}
+      </ScrollView>
     </View>
   );
 }
@@ -13,8 +41,31 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "black",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 20,
+    paddingTop: Constants.statusBarHeight,
+    padding:12
   },
+  card:{
+    gap:10,
+  },
+  image:{
+    width:107,
+    height:147,
+    borderRadius:10
+  },
+  title:{
+    color:"white",
+    fontSize:18
+  },
+  description:{
+    color:"white",
+    fontSize:12
+  },
+  score:{
+    color:"green",
+    fontSize:24,
+  }
 });
